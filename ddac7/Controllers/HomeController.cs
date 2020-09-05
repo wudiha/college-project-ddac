cc90d8896c27232fdbd200997f18ca197650d732
Tan Zhi Ning <TP042561@mail.apu.edu.my>
2020-09-06 00:12:21 +0800

    view clinic list

--- ddac7/Controllers/HomeController.cs
+++ ddac7/Controllers/HomeController.cs
@@ -5,11 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ddac7.Models;
+using ddac7.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ddac7.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ClinicAppUser> _userManager;
+        private readonly AuthDbContext _context;
+
+        public HomeController(UserManager<ClinicAppUser> userManager, AuthDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             var tt = "abc";
@@ -34,6 +46,43 @@ namespace ddac7.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult ViewClinicList()
+        {
+            var result = (from a in _context.Clinic
+                          where a.Status.Equals("Open")
+                          select new Clinic()
+                          {
+                              Id = a.Id,
+                              ClinicName = a.ClinicName,
+                              ClinicDesc = a.ClinicDesc,
+                              ContactNum = a.ContactNum,
+                              ContactEmail = a.ContactEmail,
+                              Status = a.Status,
+                              UserID = a.UserID
+                          }).ToList();
+
+            return View(result);
+        }
+
+        // View Clinic Details Page
+        public async Task<IActionResult> ClinicDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clinic = await _context.Clinic
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (clinic == null)
+            {
+                return NotFound();
+            }
+
+            return View(clinic);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
