390d7e17623bd20d146fb0115b7b906d28f4d099
Tan Zhi Ning <TP042561@mail.apu.edu.my>
2020-09-11 20:22:26 +0800

    add feedback

---
+++ ddac7/Models/Feedback.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ddac7.Models
+{
+    public class Feedback : TableEntity
+    {
+        public Feedback(int clinicID, string feedbackId)
+        {
+            this.PartitionKey = clinicID.ToString();
+            this.RowKey = feedbackId;
+        }
+
+        public Feedback() { }
+
+        [Display(Name = "Appointment ID")]
+        public string appId { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Insert your feedback:")]
+        public string FeedbackDetails { get; set; }
+
+        public string userID { get; set; }
+
+    }
+}
